d402d0e4a66534bb2028dd6d97ff1cdf0fa00278
Carlos Esteban Castro <146273220+ccaicedo09@users.noreply.github.com>
2023-11-06 18:35:40 -0500

    Create main.py
    Open the main file to take the user to the project they want to go.

---
+++ main.py
@@ -0,0 +1,9 @@
+"""
+This file works as the menu to go to each project (both command line and GUI based projects),
+through the command line. 
+"""
+
+def main():
+    print("Welcome to my projects!")
+    
+main()
