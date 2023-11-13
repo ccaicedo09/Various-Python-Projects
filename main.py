2f26536830176d0db12a3cf3ca1375cfde66ff3d
Carlos Esteban Castro <146273220+ccaicedo09@users.noreply.github.com>
2023-11-13 17:47:08 -0500

    Update main.py

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ class Menu():
         os.system("cls")
         
     def main_menu(): #Asks user which type of project do they want to see
+        print("\n------------------OPTIONS------------------\n")
         print("Would you like to see command line or GUI based projects?\n-1. Command Line Based Projects\n-2. GUI Based Projects\n-3. Exit")
         type_option = int(input("\nChoose an option ---> "))
 
@@ -31,14 +32,47 @@ class Menu():
             print("-1. Guess The Word (Spanish Version)\n-2. Exit main menu\n-3. Exit")
             project_option = int(input("\nChoose an option ---> "))
         elif type_option == 2:
-            print("-1. No projects yet\n-2. Exit main menu\n-3. Exit")
+            print("-1. Test\n-2. Exit main menu\n-3. Exit")
             project_option = int(input("\nChoose an option ---> "))
 
         return project_option
         
 def main():
-    Menu.wellcome()
+    end = False   
+    type_option = Menu.main_menu()
+    
+    if type_option == 3:
+        end = True
+        
+    while end == False:
+        project_option = Menu.choose_project(type_option)
+               
+        if type_option == 1:
+            if project_option == 1:
+                GuessTheWord_es.main()
+                break
+            elif project_option == 2:
+                main()
+            elif project_option == 3:
+                end = True
+                #os.system("cls")
+                break
+        elif type_option == 2:
+            if project_option == 1:
+                print("Test")
+            elif project_option == 2:
+                main()
+            elif project_option == 3:
+                end = True
+                break               
+        elif type_option == 3:
+            end = True
+            
+    while end == True:
+        print(f"{pyfiglet.figlet_format("See you soon!  =)", font="big")}")
+        break
 
 
 if __name__ == "__main__":
+    Menu.wellcome()
     main()
